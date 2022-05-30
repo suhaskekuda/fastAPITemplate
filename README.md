ad15f4ccd2d95af9a85236cae6dcd853c73636b4
Suhas Kekuda <suhaskekuda@FVFFN3A7Q05P.local>
2022-05-30 19:58:33 +0530

    initial app

---
+++ README.md
@@ -0,0 +1,20 @@
+# fastAPITemplate
+
+This will be template to be used for all new fastAPI API projects
+
+Getting started
+To make it easy for you to get started with application building here's a list of recommended next steps.
+
+```
+git clone https://gitlab.com/suhas.kekuda/flaskTemplate.git
+cp -r pythontemplate <ProjectName>
+cd <ProjectName>
+rm -rf .git
+```
+
+Setup environment
+The below command will create a virtual environement, and upgrade pip to latest.
+```
+bin\setup.sh setup
+```
+Your are ready to code
---
+++ app.py
@@ -0,0 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.responses import JSONResponse
+from proto import *
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(GZipMiddleware, minimum_size=512)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+log = putlog("MainExecutor")
+
+configFile = "config/app.setting.json"
+configuration = readJson(configFile)
+
+@app.get("/")
+def base():
+    return JSONResponse({"Hello": "World"})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, 
+                host=configuration["App"]["Host"],
+                port=configuration["App"]["Port"])
